cd869440c70e15e9b54daaba355c6cbf37bccd59
Paul Wang <paul.wang.is.007@gmail.com>
2021-07-31 11:43:06 -0700

    Learn what is Disposable

--- app/src/main/java/com/paul/wang/myapplication/MainActivity.kt
+++ app/src/main/java/com/paul/wang/myapplication/MainActivity.kt
@@ -3,11 +3,14 @@ package com.paul.wang.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    var disposable: Disposable? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,16 +19,28 @@ class MainActivity : AppCompatActivity() {
 
         val observer = object : Observer<String> {
             override fun onSubscribe(d: Disposable?) {
+                // Disposable here can be used to unsubscribe.
+                disposable = d
             }
+
             override fun onNext(t: String?) {
                 textView?.text = t
             }
+
             override fun onError(e: Throwable?) {
             }
+
             override fun onComplete() {
             }
         }
 
-        observable.subscribe(observer)
+        // Learning subscribeOn(), observeOn() on different Schedulers.
+        observable.subscribeOn(Schedulers.io()).observeOn(Schedulers.io()).subscribe(observer)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        disposable?.dispose()
     }
 }
