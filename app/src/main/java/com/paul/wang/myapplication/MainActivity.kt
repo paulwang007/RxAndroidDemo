d0cdd05af569bffc3725dd6648943575bf4afce0
Paul Wang <paul.wang.is.007@gmail.com>
2021-08-01 12:43:32 -0700

    Adding a DisposableObserver.

--- app/src/main/java/com/paul/wang/myapplication/MainActivity.kt
+++ app/src/main/java/com/paul/wang/myapplication/MainActivity.kt
@@ -7,10 +7,14 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.observers.DisposableObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
     var disposable: Disposable? = null
+    // A DisposableObserver essentially replaces 2 different objects, Disposable and Observer.
+    var disposableObserver: DisposableObserver<String>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,13 +38,27 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        disposableObserver = object : DisposableObserver<String>() {
+            override fun onNext(t: String?) {
+                TODO("Not yet implemented")
+            }
+            override fun onError(e: Throwable?) {
+                TODO("Not yet implemented")
+            }
+            override fun onComplete() {
+                TODO("Not yet implemented")
+            }
+        }
+
         // Learning subscribeOn(), observeOn() on different Schedulers.
-        observable.subscribeOn(Schedulers.io()).observeOn(Schedulers.io()).subscribe(observer)
+        observable.subscribeOn(Schedulers.io()).observeOn(Schedulers.io()).subscribe(disposableObserver)
     }
 
     override fun onPause() {
         super.onPause()
 
         disposable?.dispose()
+
+        disposableObserver?.dispose()
     }
 }
