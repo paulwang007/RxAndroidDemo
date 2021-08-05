a1ca918fd9f2ef15c6c76c15ecfe9e7cad724330
Paul Wang <paul.wang.is.007@gmail.com>
2021-08-04 22:55:28 -0700

    add buffer()
    Signed-off-by: Paul Wang <paul.wang.is.007@gmail.com>

--- app/src/main/java/com/paul/wang/myapplication/MainActivity.kt
+++ app/src/main/java/com/paul/wang/myapplication/MainActivity.kt
@@ -5,16 +5,13 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.ObservableOnSubscribe
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.observers.DisposableObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
-import java.util.function.Function
 
 class MainActivity : AppCompatActivity() {
     // A CompositeDisposable can be used to replace multiple Disposables or DisposableObservers are used.
     private var compositeDisposable = CompositeDisposable()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,25 +35,29 @@ class MainActivity : AppCompatActivity() {
 
         compositeDisposable.add(
             // Learning subscribeOn(), observeOn() on different Schedulers.
-            createObservable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).flatMap{
-                // Adding flatMap(), which transforms the items emitted by an Observable into Observables, then flattens the emissions from different Observables into a single Observable.
-                Observable.just('d')
-            }.concatMap {
-                // Concat is similar to flatMap, but it will wait until one observable is finished before moving on to the next observable.
-                Observable.just('a')
-            }.subscribeWith(
-                // A DisposableObserver essentially replaces 2 different objects, Disposable and Observer.
-                object : DisposableObserver<Char>() {
-                    override fun onNext(t: Char) {
-                        textView?.text = t.toString()
-                    }
+            createObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .buffer(2) // Buffer will group emissions into bundles, then emit a bundle.
+                .flatMap {
+                    // Adding flatMap(), which transforms the items emitted by an Observable into Observables, then flattens the emissions from different Observables into a single Observable.
+                    Observable.just('d')
+                }.concatMap {
+                    // Concat is similar to flatMap, but it will wait until one observable is finished before moving on to the next observable.
+                    Observable.just('a')
+                }.subscribeWith(
+                    // A DisposableObserver essentially replaces 2 different objects, Disposable and Observer.
+                    object : DisposableObserver<Char>() {
+                        override fun onNext(t: Char) {
+                            textView?.text = t.toString()
+                        }
 
-                    override fun onError(e: Throwable?) {
-                    }
+                        override fun onError(e: Throwable?) {
+                        }
 
-                    override fun onComplete() {
-                    }
-                })
+                        override fun onComplete() {
+                        }
+                    })
         )
     }
 
